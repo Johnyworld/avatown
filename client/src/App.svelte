1ebd55dd2b073bca62842a0932925879d0fde118
johnyworld <johnyworld@naver.com>
2021-11-25 00:28:20 +0900

    Alias 설정 & 테스트 scss 파일 분리

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import {onMount} from 'svelte'
+  import {HE} from '~/test'
 
   let count: number = 0
   onMount(() => {
@@ -11,57 +12,10 @@
 </script>
 
 <style lang="scss">
-  :global(body) {
-    margin: 0;
-    font-family: Arial, Helvetica, sans-serif;
-  }
-
-  .App {
-    text-align: center;
-    code {
-      background: #0002;
-      padding: 4px 8px;
-      border-radius: 4px;
-    }
-  }
-
-  .App p {
-    margin: 0.4rem;
-  }
-
-  .App-header {
-    background-color: #f9f6f6;
-    color: #333;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-  }
-
-  .App-link {
-    color: #ff3e00;
-  }
-
-  .App-logo {
-    height: 36vmin;
-    pointer-events: none;
-    margin-bottom: 3rem;
-    animation: App-logo-spin infinite 1.6s ease-in-out alternate;
-  }
-
-  @keyframes App-logo-spin {
-    from {
-      transform: scale(1);
-    }
-    to {
-      transform: scale(1.06);
-    }
-  }
+  @import './App';
 </style>
 
-<div class="App">
+<div class="App Appp">
   <header class="App-header">
     <img src="/logo.svg" class="App-logo" alt="logo"/>
     <p>Edit <code>src/App.svelte</code> and save to reload.</p>
@@ -69,6 +23,7 @@
     <p>
       <a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
         Learn Svelte
+        {HE}
       </a>
     </p>
   </header>
---
+++ client/src/test.ts
@@ -0,0 +1,1 @@
+export const HE = "Helloooooo world"
