f279085789f3c979a8507d7ba56d0c339a05d1d3
johnyworld <johnyworld@naver.com>
2021-11-25 17:55:03 +0900

    Add svelte-routing with Home and About page

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import { Router, Route, Link } from "svelte-routing";
+  import HomeScreen from '~/routes/HomeScreen.svelte';
+  import AboutScreen from '~/routes/AboutScreen.svelte';
   import {onMount} from 'svelte'
   import {HE} from '~/test'
 
@@ -15,16 +18,25 @@
   @import './App';
 </style>
 
-<div class="App Appp">
-  <header class="App-header">
-    <img src="/logo.svg" class="App-logo" alt="logo"/>
-    <p>Edit <code>src/App.svelte</code> and save to reload.</p>
-    <p>Page has been open for <code>{count}</code> seconds.</p>
-    <p>
-      <a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
-        Learn Svelte
-        {HE}
-      </a>
-    </p>
-  </header>
-</div>
+<Router url=''>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="about">About</Link>
+  </nav>
+  <Route path="/" component={HomeScreen} />
+  <Route path="/about" component={AboutScreen} /> 
+  <div class="App Appp">
+    <header class="App-header">
+      <img src="/logo.svg" class="App-logo" alt="logo"/>
+      <p>Edit <code>src/App.svelte</code> and save to reload.</p>
+      <p>Page has been open for <code>{count}</code> seconds.</p>
+      <p>
+        <a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
+          Learn Svelte
+          {HE}
+        </a>
+      </p>
+    </header>
+  </div>
+
+</Router>
